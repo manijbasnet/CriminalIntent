7d274c4ccb2365d9fd8d4ebe862f16369bd11c19
Nij <manij07@hotmail.co.uk>
2019-09-21 11:16:36 +0100

    rename MainActivity to CrimeActivity

--- app/src/main/java/com/example/criminalintent/CrimeActivity.kt
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.kt
@@ -3,10 +3,10 @@ package com.example.criminalintent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CrimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_crime)
     }
 }
