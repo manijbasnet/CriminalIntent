469975b8645fe8cea7f47919cc12195f23e2e324
Nij <manij07@hotmail.co.uk>
2019-09-21 12:26:21 +0100

    created new fragment for crime detail view and add it to the feagment container in crime activity

--- app/src/main/java/com/example/criminalintent/CrimeActivity.kt
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.kt
@@ -1,12 +1,21 @@
 package com.example.criminalintent
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
 
-class CrimeActivity : AppCompatActivity() {
+class CrimeActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crime)
+
+        var fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if(fragment == null){
+            fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
